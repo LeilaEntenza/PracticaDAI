b72a738b7eb9fc1e51e4997e50a6e0cf07abbc75
LEILA ALUMNE ENTENZA CHIDERSKI <48385438@ort>
2025-04-21 10:38:28 -0300

    Trabajo casi terminado

--- index.js
+++ index.js
@@ -20,22 +20,55 @@ app.get('/numero', (req, res) =>{
     res.send(42);
 });
 
-let usuario = {
-    "nombre":"ana", "edad":"25"
-}
+
 
 app.get('/usuario', (req, res) => {
+    let usuario = {
+        nombre:"ana", edad:"25"
+    }
   res.json(usuario);  
 });
 
-let productos = ["Mouse", "Teclado", "Monitor"];
+
 
 app.get('/productos', (req, res) => {
+    let productos = ["Mouse", "Teclado", "Monitor"];
     res.json(productos);
 });
 
-let materias = [{"nombre": "matemática"}, {"nombre":"lengua"}]
+
 
 app.get('/materias', (req, res) => {
+    let materias = [{"nombre": "matemática"}, {"nombre":"lengua"}]
     res.json(materias);
+})
+
+/*
+- Incorporar un array de objetos "Persona"
+    - Armar un endpoint que agregue una nueva persona
+    - Armar un endpoint que muestre los datos del array persona
+
+- Armar un endpoint para borrar a una persona en particular
+    -Chequear que la persona a borrar exista. Sino mostrar un mensaje de "Persona no encontrada"
+
+*/
+
+let personas = [];
+app.post('/persona', (req, res) => {
+    let nombre = req.body;
+    personas.push(nombre);
+    res.send(nombre);
+})
+
+app.get('/personas', (req, res) => {
+    res.json(personas);
+})
+
+app.delete('/eliminar:indice', (req, res) => {
+    const indice = Number(req.params.indice);
+    if (isNaN(indice) || personas[indice] == null){
+        return res.send("Persona no encontrada")
+    }
+    personas.splice(indice, 1);
+    res.send("Persona eliminada");
 })
